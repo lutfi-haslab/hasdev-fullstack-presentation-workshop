ea1ea9b4f8c50b922d3063344b4e325aa58d51c0
lutfi-haslab <work.lutfiikbalmajid@gmail.com>
2023-05-19 14:53:38 +0700

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,21 @@
-import { Outlet, ReactLocation, Router } from "@tanstack/react-location";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import About from "./pages/About";
 import Home from "./pages/Home";
-const location = new ReactLocation();
 
-const App = () => {
-  return (
-    <Router
-      location={location}
-      routes={[
-        {
-          path: "/",
-          element: <About />,
-        },
-        {
-          path: "about",
-          element: <About />,
-        },
-        {
-          path: "/slide/home",
-          element: <Home />,
-        },
-      ]}
-    >
-      <Outlet />
-    </Router>
-  );
-};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
